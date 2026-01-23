e3eddb9f8119a449788137da2ebfc7e41ddcbc8b
Samuel McKey <sammckey24@gmail.com>
2026-01-23 12:57:49 -0500

    Adding blueprints to __init__.py

--- Module_1/__init__.py
+++ Module_1/__init__.py
@@ -1,10 +1,22 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def home():
+#     return "Mamba Wamba Rocks My Socks Off!"
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=8000, debug=True)
+
+
+
 from flask import Flask
 
-app = Flask(__name__)
+from board import pages
 
-@app.route("/")
-def home():
-    return "Mamba Wamba Rocks My Socks Off!"
+def create_app():
+    app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.register_blueprint(pages.bp)
+    return app
