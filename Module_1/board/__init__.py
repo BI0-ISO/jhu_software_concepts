a6a53ed1ca7365403ef5df1354f8347762e1bdbc
Samuel McKey <sammckey24@gmail.com>
2026-01-24 01:25:19 -0500

    Module_1 update to get pages to work

--- Module_1/board/__init__.py
+++ Module_1/board/__init__.py
@@ -10,13 +10,10 @@
 #     app.run(host="0.0.0.0", port=8000, debug=True)
 
 
-
 from flask import Flask
-
-from board import pages
+from .board.pages import bp 
 
 def create_app():
     app = Flask(__name__)
-
-    app.register_blueprint(pages.bp)
+    app.register_blueprint(bp)
     return app
