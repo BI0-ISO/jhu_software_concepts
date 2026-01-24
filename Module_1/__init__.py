78ca1c1e12ca3847f9e094e48bc1cec4735d3c4a
Samuel McKey <sammckey24@gmail.com>
2026-01-24 00:52:00 -0500

    Updating Module_1 with static, and html

---
+++ Module_1/__init__.py
@@ -0,0 +1,22 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def home():
+#     return "Mamba Wamba Rocks My Socks Off!"
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=8000, debug=True)
+
+
+
+from flask import Flask
+
+from Module_1 import pages
+
+def create_app():
+    app = Flask(__name__)
+
+    app.register_blueprint(pages.bp)
+    return app
