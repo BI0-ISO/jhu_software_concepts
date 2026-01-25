9af7f010a64f49ab2d68e07b7af57e857cd6076e
Samuel McKey <Samuel.McKey@jhuapl.edu>
2026-01-25 15:12:24 -0500

    Update __init__.py

--- Module_1/__init__.py
+++ Module_1/__init__.py
@@ -2,8 +2,14 @@ from flask import Flask
 from Module_1.board.pages import bp
 import os
 
+# Application Factory 
 def create_app():
+
+    
     base_dir = os.path.abspath(os.path.dirname(__file__))
+
+    # Brings us to the correct directory for Flask 
+    
     project_root = os.path.dirname(base_dir)
 
     app = Flask(
