8368db14db80c45ab5d9eb28b7057d3ac467c0a5
Samuel McKey <sammckey24@gmail.com>
2026-01-24 22:16:10 -0500

    Changing __init__.py to work with the reorganization

--- run.py
+++ run.py
@@ -3,4 +3,8 @@ from Module_1 import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",  # or "127.0.0.1"
+        port=8080,
+        debug=True
+    )
