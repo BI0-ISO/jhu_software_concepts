3d035ee8b4df6d22d774955f8373e016059f6581
Samuel McKey <sammckey24@gmail.com>
2026-01-24 22:01:48 -0500

    Incorporating python run.py capabilities

--- Module_1/__init__.py
+++ Module_1/__init__.py
@@ -1,21 +1,28 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route("/")
-# def home():
-#     return "Mamba Wamba Rocks My Socks Off!"
-
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=8000, debug=True)
-
 from flask import Flask
+from Module_1.board.pages import bp
+import os
 
 def create_app():
-    app = Flask(__name__)
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    project_root = os.path.dirname(base_dir)
 
-    # Import blueprint here, inside the function to avoid circular imports
-    from Module_1.board.pages import bp
+    app = Flask(
+        __name__,
+        template_folder=os.path.join(project_root, "templates"),
+        static_folder=os.path.join(project_root, "static")
+    )
+
     app.register_blueprint(bp)
-
     return app
+
+# """ from flask import Flask
+
+# def create_app():
+#     app = Flask(__name__)
+
+#     # Import blueprint here, inside the function to avoid circular imports
+#     from Module_1.board.pages import bp
+#     app.register_blueprint(bp)
+
+#     return app
+#  """
