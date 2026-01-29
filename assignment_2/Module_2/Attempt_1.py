50502600cdf9e83b804db625acfb86625ab2e6e4
Samuel McKey <sammckey24@gmail.com>
2026-01-28 19:54:02 -0500

    Fixing SSL

--- assignment_2/Module_2/Attempt_1.py
+++ assignment_2/Module_2/Attempt_1.py
@@ -2,37 +2,39 @@ import urllib3
 from bs4 import BeautifulSoup
 import json
 
+# Create a PoolManager instance
+http = urllib3.PoolManager()
 
-http = urllib3.PoolManager(cert_reqs=ssl.CERT_NONE)
+# Fetch the webpage
+url = "https://www.thegradcafe.com/result/900000"
+response = http.request("GET", url)
 
-url = http.request("GET", "https://www.thegradcafe.com/result/900000")
-html = url.data.decode("utf-8")
+# Decode bytes to string
+html = response.data.decode("utf-8")
 
-
-
-with urllib.request.urlopen(url) as response:
-    html = response.read()
-
+# Parse HTML with BeautifulSoup
 soup = BeautifulSoup(html, "html.parser")
 
+# Now you can use soup to find elements
+print(soup.title.text)  # Example: prints the page title
 
-test_df = {
-        "Program Name": ,
-        "University": ,
-        "Comments": ,
-        "Date of Information Added to Grad Café": ,
-        "URL link to applicant entry": ,
-        "Applicant Status": ,
-        "Accepted: Acceptance Date": ,
-        "Rejected: Rejection Date": ,
-        "Semester and Year of Program Start": ,
-        "International / American Student": ,
-        "GRE Score": ,
-        "GRE V Score": ,
-        "Masters or PhD": ,
-        "GPA": ,
-        "GRE AW": 
-    }
+# test_df = {
+#         "Program Name": ,
+#         "University": ,
+#         "Comments": ,
+#         "Date of Information Added to Grad Café": ,
+#         "URL link to applicant entry": ,
+#         "Applicant Status": ,
+#         "Accepted: Acceptance Date": ,
+#         "Rejected: Rejection Date": ,
+#         "Semester and Year of Program Start": ,
+#         "International / American Student": ,
+#         "GRE Score": ,
+#         "GRE V Score": ,
+#         "Masters or PhD": ,
+#         "GPA": ,
+#         "GRE AW": 
+#     }
 
 
 
