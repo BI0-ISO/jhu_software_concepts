aea11d68dec4549ca6a9bc9812eb10dc5a9d722e
Samuel McKey <sammckey24@gmail.com>
2026-01-28 21:21:26 -0500

    Grabbing a single applicant info

--- assignment_2/Module_2/Attempt_1.py
+++ assignment_2/Module_2/Attempt_1.py
@@ -1,40 +1,53 @@
 import urllib3
 from bs4 import BeautifulSoup
 import json
+import re
 
-# Create a PoolManager instance
+
 http = urllib3.PoolManager()
 
-# Fetch the webpage
+
 url = "https://www.thegradcafe.com/result/900000"
 response = http.request("GET", url)
 
-# Decode bytes to string
+
 html = response.data.decode("utf-8")
 
-# Parse HTML with BeautifulSoup
+
 soup = BeautifulSoup(html, "html.parser")
 
-# Now you can use soup to find elements
-print(soup.title.text)  # Example: prints the page title
+text = soup.get_text(separator="\n")
 
-# test_df = {
-#         "Program Name": ,
-#         "University": ,
-#         "Comments": ,
-#         "Date of Information Added to Grad Café": ,
-#         "URL link to applicant entry": ,
-#         "Applicant Status": ,
-#         "Accepted: Acceptance Date": ,
-#         "Rejected: Rejection Date": ,
-#         "Semester and Year of Program Start": ,
-#         "International / American Student": ,
-#         "GRE Score": ,
-#         "GRE V Score": ,
-#         "Masters or PhD": ,
-#         "GPA": ,
-#         "GRE AW": 
-#     }
+print(text)
 
+print(soup.title.text)  
+
+patterns = {
+    "Institution": r"Institution\s*(.*)",
+    "Degree Type": r"Degree Type\s*(.*)",
+    "Decision": r"Decision\s*(.*)",
+    "Undergrad GPA": r"Undergrad GPA\s*(.*)",
+    "Program": r"Program\s*(.*)",
+    "Notes": r"Notes\s*(.*)",
+    "Notification": r"Notification\s*(.*)",
+    "GRE General": r"GRE General:\s*(\d+)",
+    "GRE Verbal": r"GRE Verbal:\s*(\d+)",
+    "Analytical Writing": r"Analytical Writing:\s*(\d+\.?\d*)",
+    "Applicant Link": r"Applicant Entry:\s*(https?://[^\s]+)"
+}
 
 
+applicant_data = {}
+
+# This will act as a for loop to go through the dictionary
+for field, pattern in patterns.items():
+    # Using the re library, I will search for the pattern selected from the for loop iteration within the text html
+    match = re.search(pattern, text)
+    if match:
+        # strip removes the spaces besides the word 
+        applicant_data[field] = match.group(1).strip()
+    else:
+        # I want the field to be filled with something, so if there is nothing, it fills with "NA"
+        applicant_data[field] = "NA" # If field not found
+
+print (applicant_data)
