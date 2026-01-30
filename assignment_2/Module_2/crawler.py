58152df960269563248e630c5a819a3ae6bd8b67
Samuel McKey <sammckey24@gmail.com>
2026-01-30 16:49:03 -0500

    Update to crawler

--- assignment_2/Module_2/crawler.py
+++ assignment_2/Module_2/crawler.py
@@ -22,3 +22,42 @@ def scrape_data(start_entry, end_entry):
     # check that sees if 1) this page actually loads, and 2) if the information loads 
     # I found that when I vary the {entry_#} manually, sometimes the information doesn't load 
 
+    # I can make a for loop to go through each applicant entry 
+
+    for entry_num in range(start_entry,end_entry):
+
+        #First I access an applicant's entry 
+
+        url = f"https://www.thegradcafe.com/result/{entry_num}"
+
+        # Next I make a HTTP request to access this information using this url provided
+        try: 
+            response = http.request("GET", url)
+
+            # Now I do a check to make sure the site works properly, and if so, save the information 
+
+            # Something I found when trying to break this code, is that when you input an entry that is not valid 
+            # the website still works but only a notifcation entry is provided and spits out 31/12/1969
+
+            if response.status == 200:
+
+                print(response.status)
+
+                raw_data.append({
+                        "url": url,
+                        "html": response.data.decode("utf-8")
+                        })
+                
+
+        # If there is something that is wrong with this site, just keep on to the next one        
+        except: 
+            continue 
+
+    return raw_data
+    
+
+
+test = scrape_data(9000000,9000001)
+
+
+
